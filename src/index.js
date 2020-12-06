eb475f6526452cf43c320c7bdb2d212e905a6b37
Kapic007 <48770409+Kapic007@users.noreply.github.com>
2020-12-06 20:35:01 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const movie = {
+  title: "Avengers: Infinity War",
+  vote_average: 8.5,
+  image: "https://image.tmdb.org/t/p/w500/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+  overview:
+    "lkffjkfdjgkj k;fjg fdjg kldfjg klfdj glkdfg jdfklgj klfdgj kldfjg dfjg kjdfgk jdfkg jdklgj dkjgkdfgjk jgk j"
+};
+
+function Image(props) {
+  console.log("Image props = ", props);
+  return <img src={props.src} alt={props.alt} />;
+}
+
+/*function MovieItem(props) {
+  console.log("MovieItem props = ", props);
+  const {
+    data: { title, vote_average, image }
+  } = props;
+  return (
+    <div>
+      <Image src={image} alt ={title} />
+      <p>{title}</p>
+      <p>{vote_average}</p>
+    </div>
+  );
+}*/
+
+class MovieItem extends React.Component {
+  render() {
+    const {
+      data: { title, vote_average, image }
+    } = this.props;
+    console.log(this);
+    return (
+      <div>
+        <Image src={image} alt={title} />
+        <p>{title}</p>
+        <p>{vote_average}</p>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <MovieItem data={movie} />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
